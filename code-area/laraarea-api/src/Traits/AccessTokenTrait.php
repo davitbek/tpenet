c232f6a4710f51ab1f400a0a964ddcfe200ff8c5
davitbek <davitzeynalyan1991@gmail.com>
2021-01-19 23:03:57 +0400

    dd

---
+++ code-area/laraarea-api/src/Traits/AccessTokenTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace LaraAreaApi\Traits;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Request;
+use LaraAreaApi\Exceptions\ApiAuthTokenException;
+use Laravel\Passport\Passport;
+
+trait AccessTokenTrait
+{
+    public function getLoginToken()
+    {
+
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     * @throws ApiAuthTokenException
+     */
+    public function getLoginTokenByCredentials($data)
+    {
+        if (!empty($data[$this->queryParams['remember_me']])) {
+            Passport::tokensExpireIn(now()->addDays(config('laraarea_api.auth.remember_me_days', 30)));
+        } elseif (!empty($data[$this->queryParams['remember_days']])) {
+            Passport::tokensExpireIn(now()->addDays($data[$this->queryParams['remember_days']]));
+        }
+dd(12);
+        return $tokens;
+    }
+
+    /**
+     * @param $user
+     * @param int $days
+     * @param false $forceLogin
+     * @return mixed
+     */
+    public function createAccessToken($user, $days = 30)
+    {
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+        $token->expires_at = Carbon::now()->addDays($days);
+        $token->save();
+        return $tokenResult;
+    }
+
+    protected function createAccessTokenByCredentials($userName, $password)
+    {
+        $host = request()->getHost(); // TODO improve
+        $tokenRequest = Request::create(
+            '/oauth/token',
+            'post', [
+            'grant_type' => 'password',
+            'client_id' => env('O_AUTH_CLIENT_ID'),
+            'client_secret' => env('O_AUTH_CLIENT_SECRET'),
+            'username' => $userName,
+            'password' => $password,
+            'scope' => '',
+        ],
+            [],
+            [],
+            [
+                'SERVER_NAME' => $host,
+                'HTTP_HOST' => $host,
+            ]
+        );
+        $tokenRequest = clone $tokenRequest;
+        $response = app()->handle($tokenRequest);
+        return json_decode((string) $response->getContent(), true);
+    }
+}
