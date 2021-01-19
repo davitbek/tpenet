beee3d32beabe6e99e799a843d6e35164b0a761a
davitbek <davitzeynalyan1991@gmail.com>
2021-01-20 00:10:43 +0400

    dd

--- code-area/laraarea-api/config/laraarea_api.php
+++ code-area/laraarea-api/config/laraarea_api.php
@@ -21,6 +21,8 @@ return [
     'auth' => [
         'private_key' => env('PASSPORT_PRIVATE_KEY'),
         'public_key' => env('PASSPORT_PUBLIC_KEY'),
+        'client_id' => env('PASSPORT_CLIENT_ID'),
+        'client_secret' => env('PASSPORT_CLIENT_SECRET'),
         'table' => 'users',
         'scope' => [
             'criteria' => [
